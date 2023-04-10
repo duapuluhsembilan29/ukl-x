1957b0cd6e5b46575c7122859aeb02a40d59849b
Mohammad Alka. R <99939707+duapuluhsembilan29@users.noreply.github.com>
2023-04-10 13:09:21 +0700

    Create Petugas.java

---
+++ Laundry/Petugas.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laundry;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Fonda
+ */
+public class Petugas implements User{
+    private ArrayList<String> namaPetugas = new ArrayList<String>();
+    private ArrayList<String> alamat = new ArrayList<String>();
+    private ArrayList<String> telepon = new ArrayList<String>();
+    
+    public Petugas(){
+        this.namaPetugas.add("Gyda Twinsz");
+        this.alamat.add("Mekkah");
+        this.telepon.add("081456789323");
+        
+        this.namaPetugas.add("Naufal Twinsz");
+        this.alamat.add("Madinah");
+        this.telepon.add("08212345679");
+    }
+
+    @Override
+    public void setNama(String nama) {
+        this.namaPetugas.add(nama);
+    }
+
+    @Override
+    public void setAlamat(String alamat) {
+        this.alamat.add(alamat);
+    }
+
+    @Override
+    public void setTelepon(String telepon) {
+        this.telepon.add(telepon);
+    }
+
+    @Override
+    public String getNama(int id) {
+        return this.namaPetugas.get(id);
+    }
+
+    @Override
+    public String getAlamat(int id) {
+        return this.alamat.get(id);
+    }
+
+    @Override
+    public String getTelepon(int id) {
+        return this.telepon.get(id);
+    }
+    
+    public void tampilPetugas(){
+        int n = this.namaPetugas.size();
+        for(int i=0;i<n;i++){
+            System.out.println("------------------------");
+            System.out.println("Nama    = "+getNama(i));
+            System.out.println("Alamat  = "+getAlamat(i));
+            System.out.println("Telepon = "+getTelepon(i));
+        }
+    }
+}
