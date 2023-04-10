3ccf86e45e24169959410d5b604b24a6a79df144
Mohammad Alka. R <99939707+duapuluhsembilan29@users.noreply.github.com>
2023-04-10 13:37:40 +0700

    Create Siswa.java

---
+++ Perpustakaan/Siswa.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perpustakaan;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alkaa
+ */
+public class Siswa implements User{
+    private ArrayList<String> namaSiswa = new ArrayList<String>();
+    private ArrayList<String> alamat = new ArrayList<String>();
+    private ArrayList<String> telepon = new ArrayList<String>();
+    private ArrayList<Boolean> status = new ArrayList<Boolean>();
+    //auto bekerja
+    public Siswa(){
+        this.namaSiswa.add("Bintang");
+        this.alamat.add("Italy");
+        this.telepon.add("084657898987");
+        this.status.add(true);
+        
+        this.namaSiswa.add("Alkaa");
+        this.alamat.add("Spanyol");
+        this.telepon.add("082143362962");
+        this.status.add(true);
+    }
+
+    @Override
+    public void setNama(String nama) {
+        this.namaSiswa.add(nama);
+    }
+
+    @Override
+    public void setAlamat(String alamat) {
+        this.alamat.add(alamat);
+    }
+
+    @Override
+    public void setTelepon(String telepon) {
+        this.telepon.add(telepon);
+    }
+
+    @Override
+    public String getNama(int id) {
+        return this.namaSiswa.get(id);
+    }
+
+    @Override
+    public String getAlamat(int id) {
+        return this.alamat.get(id);
+    }
+
+    @Override
+    public String getTelepon(int id) {
+        return this.telepon.get(id);
+    }
+    public void setStatus(int id, boolean status){
+        this.status.set(id, status);
+    }
+    public boolean getStataus(int id){
+        return this.status.get(id);
+    }
+    public void tampilkanSiswa(){
+        int n = this.namaSiswa.size();
+        for(int i = 0;i<n;i++){
+            System.out.println("--------------------------");
+            System.out.println("Nama    = "+getNama(i));
+            System.out.println("Alamat  = "+getAlamat(i));
+            System.out.println("Telepon = "+getTelepon(i));
+        }
+        
+    }
+}
